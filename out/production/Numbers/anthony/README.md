bb25bab5884a5fd337e14a55d30da5775ef7770b
Anthony <amcmullen9291@gmail.com>
2021-10-15 11:37:48 -0400

    finished NumberGame

---
+++ out/production/Numbers/anthony/README.md
@@ -0,0 +1,3 @@
+#Java Programs
+
+root directory for Java Bootcamp programs
--- src/anthony/NumberGame.java
+++ src/anthony/NumberGame.java
@@ -1,7 +1,36 @@
 package anthony;
 
+import java.util.Scanner;
+
 public class NumberGame {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int secretNumber;
+        int userGuess;
+        boolean bingo = false;
+
+        int random = (int) (Math.random() * 20 + 1);
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Welcome to \"Guess the Number\"! ");
+        System.out.println("Choose a number; any number:");
+
+        secretNumber = keyboard.nextInt();
+        System.out.println("good choice!");
+        System.out.println("");
+        System.out.println("NOW ITS MY TURN. Guess a number between 1 and 20.");
+        System.out.println("***FYI, my number for this round is " + random + ". (Ain't I a stinker?)");
+
+        while (!bingo) {
+            System.out.print("Guess My Number.:");
+            userGuess = keyboard.nextInt();
+                if (userGuess == random) {
+                    bingo = true;
+                    System.out.println("Bingo! I will give you " + secretNumber + " cheeseburgers. Let's play again?");
+                } else if (userGuess < random) {
+                    System.out.println("Higher.");
+                } else if (userGuess > random) {
+                    System.out.println("Lower.");
+                }
+            }
     }
 }
