c6be50509acb65a9d7cfc921794db50e5a6aa0ce
Anthony <amcmullen9291@gmail.com>
2021-10-16 07:37:34 -0400

    fixed char input

--- src/anthony/HangmanGame.java
+++ src/anthony/HangmanGame.java
@@ -2,7 +2,9 @@ package anthony;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-// Note: the while loop is currently in an infinite loop. DO NOT RUN YET
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class HangmanGame {
     public static void main(String[] args) {
         String[] words = {"puppy", "vacation", "football", "oxymoron", "hyperbole", "Champagne",
@@ -14,24 +16,37 @@ public class HangmanGame {
 
         System.out.println("JAVA - HANGMAN (written by Anthony)");
         System.out.println(".");
+
+        for(int blankSpace=0; blankSpace<letters.length; blankSpace++){
+            letters[blankSpace] = '_';
+        }
+        System.out.println(letters);
+
         int lives = 5;
         Scanner scanner= new Scanner(System.in);
         while(lives > 0){
             System.out.print("Guesses remaining: ");
-            for (int i =0; i > lives; i++){
+            for (int i =0; i < lives; i++){
                 System.out.print("X");
             }
 
             System.out.println();
 
             System.out.println("Guess a letter.");
-            if(scanner.hasNextLine()) {
-                String usersGuess = scanner.nextLine();
+            String usersGuess = scanner.nextLine();
+
+            String patternString = ".*[A-Za-z]{1}.*";
+            Pattern pattern = Pattern.compile(patternString);
+            Matcher matcher = pattern.matcher(usersGuess);
+            boolean matches = matcher.matches();
+            if(matches) {
                 char letter = usersGuess.charAt(0);
             }else{
                 System.out.println("That was not a letter. Please select a letter, A-Z.");
-                if(scanner.hasNextLine()) {
-                    String usersGuess = scanner.nextLine();
+                usersGuess = scanner.nextLine();
+                Matcher matcher2 = pattern.matcher(usersGuess);
+                boolean matches2 = matcher2.matches();
+                if(matches2) {
                     char letter = usersGuess.charAt(0);
                 }else{
                     System.out.println("GOODBYE!");
@@ -39,6 +54,7 @@ public class HangmanGame {
                 }
             }
         }
+
     }
 }
 
