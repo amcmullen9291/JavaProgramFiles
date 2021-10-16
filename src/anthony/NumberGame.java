38992d3df0ee91d74b3a22f24e0c15ad49ef53d6
Anthony <amcmullen9291@gmail.com>
2021-10-16 13:39:23 -0400

    updated NumberGame

--- src/anthony/NumberGame.java
+++ src/anthony/NumberGame.java
@@ -21,12 +21,15 @@ public class NumberGame {
         System.out.println("NOW ITS MY TURN. Guess a number between 1 and 20.");
         System.out.println("***FYI, my number for this round is " + random + ". (Ain't I a stinker?)");
 
+        String[] foods = {"bananas", "corndogs", "tacos and burritos", "a bottle of mouth wash", "sticks of chewing gum", "corn cob pipes"};
+        int randomFood = (int) (Math.random() * 6 + 1);
+        String endFood = foods[randomFood];
         while (!bingo) {
             System.out.print("Guess My Number.:");
             userGuess = keyboard.nextInt();
             if (userGuess == random) {
                 bingo = true;
-                System.out.println("Bingo! I will give you " + secretNumber + " cheeseburgers. Let's play again?");
+                System.out.println("Bingo! I will give you " + secretNumber + " "+ endFood + ". Let's play again?");
             } else if (userGuess < random) {
                 System.out.println("Higher.");
             } else if (userGuess > random) {
