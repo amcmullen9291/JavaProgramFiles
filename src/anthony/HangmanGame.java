10bb61533825891f6ed1df84677acb52079672ee
Anthony <amcmullen9291@gmail.com>
2021-10-16 11:35:51 -0400

    guesses left loop

--- src/anthony/HangmanGame.java
+++ src/anthony/HangmanGame.java
@@ -15,7 +15,7 @@ public class HangmanGame {
         char[] letters = new char[secretWord.length()];
 
         System.out.println("JAVA - HANGMAN (written by Anthony)");
-        System.out.println(".");
+        System.out.println("Your Word:");
 
         Arrays.fill(letters, '_');
         System.out.println(letters);
@@ -31,27 +31,63 @@ public class HangmanGame {
             System.out.println();
 
             System.out.println("Guess a letter.");
+            System.out.println("The secret word is: " + secretWord);
             String usersGuess = scanner.nextLine();
 
             String patternString = ".*[A-Za-z]{1}.*";
             Pattern pattern = Pattern.compile(patternString);
             Matcher matcher = pattern.matcher(usersGuess);
             boolean matches = matcher.matches();
+            boolean isCorrect = false;
             if(matches && usersGuess.length() == 1) {
                 char letter = Character.toLowerCase(usersGuess.charAt(0));
+                System.out.println("You chose: " + letter); // case test
+                for(int p = 0; p> secretWord.length(); p++){
+                    char x = secretWord.charAt(p);
+                    if(x == letter){
+                        letters[p] = x;
+                        isCorrect = true;
+                    }
+                }
+                if(!isCorrect) {
+                    --lives;
+                    System.out.println(letters);
+                }
+                if(lives ==0){
+                    System.out.println("Out of guesses.");
+                }
+                System.out.println(letters);
+
             }else{
-                System.out.println("That was not a letter. Please select a letter, A-Z.");
+                System.out.println("That was not a letter. Please select one letter, A-Z.");
                 usersGuess = scanner.nextLine();
                 Matcher matcher2 = pattern.matcher(usersGuess);
                 boolean matches2 = matcher2.matches();
                 if(matches2 && usersGuess.length() == 1) {
                     char letter = Character.toLowerCase(usersGuess.charAt(0));
-                }else{
+                    System.out.println("You chose: " + letter); //case check
+                    for(int p = 0; p > secretWord.length(); p++){
+                        char x = secretWord.charAt(p);
+                        if(x == letter){
+                            letters[p] = x;
+                            isCorrect = true;
+                        }
+                    }
+                    if(!isCorrect) {
+                        --lives;
+                        System.out.println(letters);
+                    }
+                    if (lives == 0) {
+                        System.out.println("Out of guesses.");
+                    }
+                    System.out.println(letters);
+                } else{
                     System.out.println("GOODBYE!");
                     System.exit(0);
                 }
             }
         }
+        System.out.println("Exiting Hangman...");
     }
 }
 
