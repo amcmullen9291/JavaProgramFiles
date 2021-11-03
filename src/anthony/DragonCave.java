d61e61f99b02e790a183d77fbd0730ed65f0d588
Anthony <amcmullen9291@gmail.com>
2021-11-03 08:57:03 -0400

    added try/catch statement to dragonCave

--- src/anthony/DragonCave.java
+++ src/anthony/DragonCave.java
@@ -13,22 +13,27 @@ public class DragonCave {
         System.out.println("is greedy and hungry and will");
         System.out.println("EAT YOU ON SIGHT.");
         System.out.println("Which cave will you go into? (1 or 2)");
-        int computerChoice;
+        int computerChoice = 0;
 
         Scanner choice = new Scanner(System.in);
         int userChoice = choice.nextInt();
+        try {
 
-        if(Math.random() < 0.5) {
-            computerChoice = 2;
-        }else{
-            computerChoice = 1;
+            if (Math.random() < 0.5) {
+                computerChoice = 2;
+            } else {
+                computerChoice = 1;
+            }
+
+            if (userChoice == computerChoice) {
+                System.out.println("You are safe. Be well.");
+            } else {
+                System.out.println("You will be missed.");
+            }
+        }catch(Exception e){
+            System.out.println("undetermined outcome. Try again.");
         }
 
-        if(userChoice == computerChoice){
-            System.out.println("You are safe. Be well.");
-        }else {
-            System.out.println("You will be missed.");
-        }
         System.out.println("computer's choice:" + computerChoice);
     }
 }
